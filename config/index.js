b02c7016700d680e7c48ccdde15282d8ef721eb4
Phil Livermore <phil.livermore@outlook.com>
2021-10-21 17:22:44 +0100

    change port

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
-  port: process.env.HTTP_PORT || 3000,
+  port: process.env.port || 3000,
   mongoUrl: 'mongodb+srv://PhilLiv:Lmr1979*@cluster0.4m44e.mongodb.net/test',
   mongoOptions: {
     useNewUrlParser: true,
