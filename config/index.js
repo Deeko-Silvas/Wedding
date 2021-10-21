333865720bdd5b20c59f93f8df1225854db0cad9
Phil Livermore <phil.livermore@outlook.com>
2021-10-21 18:38:49 +0100

    fix mongo url

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 'use strict';
+const secrets = require('../secrets')
+
 
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.HTTP_PORT || 3000,
-  mongoUrl: '',
+  mongoUrl: process.env.MONGO_URL || secrets.mongoURL,
   mongoOptions: {
     useNewUrlParser: true,
     useCreateIndex: true,
