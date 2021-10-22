161e36cf9e6f695c71a709bd2808b896f1ef15cf
Phil Livermore <phil.livermore@outlook.com>
2021-10-22 17:34:38 +0100

    comment out secrets

--- config/index.js
+++ config/index.js
@@ -1,11 +1,10 @@
 'use strict';
-const secrets = require('../secrets')
-
+// const secrets = require('../secrets.js');
 
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.HTTP_PORT || 3000,
-  mongoUrl: process.env.MONGO_URL || secrets.mongoURL,
+  mongoUrl: process.env.MONGO_URL,
   mongoOptions: {
     useNewUrlParser: true,
     useCreateIndex: true,
